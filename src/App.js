17117f0022ff893b861f67867b7474d65bf27092
Sravyagit <sravyagatika2002@gmail.com>
2024-12-21 10:35:58 +0530

    adding 3 files together

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Orange from './Orange';
+import Apple from './Apple';
+
+function App() {
+  return (
+    <div className="App">
+     <Orange />
+     <Apple name="Sravya" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Orange.jsx
@@ -0,0 +1,8 @@
+function Orange()
+{
+    return(
+        <h1>Gatika Sravya</h1>
+    )
+}
+
+export default Orange;
